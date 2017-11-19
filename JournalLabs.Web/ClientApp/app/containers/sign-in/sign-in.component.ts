580d9bc2b22373b164d96956c8dea67858879076
Sas.Vlad <sasvlad1@gmail.com>
2017-11-19 16:46:18 +0200

    fixed nav menu ith multiple active links and rusification all site

--- JournalLabs.Web/ClientApp/app/containers/sign-in/sign-in.component.ts
+++ JournalLabs.Web/ClientApp/app/containers/sign-in/sign-in.component.ts
@@ -5,18 +5,22 @@ import { Router, CanActivate, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'sign-in',
   template: `
-      <h1>Input your Email and password to sign in into site</h1>
+      <h1>Войти как преподователь:</h1>
       <br />
-      <p>Input Email:</p>
+      <p>Введите логин</p>
       <input type="text" [(ngModel)]="teacherModel.Login" name="Login"/>
       <br />
-      <p>Input password:</p>
+      <p>Введите пароль:</p>
       <input type="text" [(ngModel)]="teacherModel.Password" name="Password"/>
       <br />
-      <button (click)="SignIn()">Sign in</button>
+      <button (click)="SignIn()">Войти</button>
   `
 })
 export class SignInComponent {
+  /*Input Email:*/
+  /*Input your Email and password to sign in into site*/
+  /*Input password:*/
+  /*Sign in*/
   public teacherModel: User = { Id: "", Login: "", Password: "", Role: "" };
 
   constructor(public router: Router,
