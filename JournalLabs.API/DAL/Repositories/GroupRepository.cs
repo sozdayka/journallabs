d337464bcee6a39e60e3cb528b8c4f79003fa784
Sas.Vlad <sasvlad1@gmail.com>
2018-02-27 10:29:27 +0200

    Add cathedra and group Services

---
+++ JournalLabs.API/DAL/Repositories/GroupRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JournalLabs.API.DAL.Repositories
+{
+    public class GroupRepository
+    {
+    }
+}
